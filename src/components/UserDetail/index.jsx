08df246444aaed9072d6b3ef302ae3e4d3488fa4
ManhVD2004 <141122890+ManhVD2004@users.noreply.github.com>
2025-11-22 11:34:00 +0700

    Update index.jsx

--- src/components/UserDetail/index.jsx
+++ src/components/UserDetail/index.jsx
@@ -1,24 +1,40 @@
-import React from "react";
-import {Typography} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Typography } from "@mui/material";
 
 import "./styles.css";
-import {useParams} from "react-router-dom";
+import fetchModel from "../../lib/fetchModelData";
 
-/**
- * Define UserDetail, a React component of Project 4.
- */
 function UserDetail() {
-    const user = useParams();
-    return (
-        <>
-          <Typography variant="body1">
-            This should be the UserDetail view of the PhotoShare app. Since it is
-            invoked from React Router the params from the route will be in property match.
-            So this should show details of user: {user.userId}.
-            You can fetch the model for the user from models.userModel.
-          </Typography>
-        </>
-    );
+  const { userId } = useParams();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    fetchModel(`/api/user/${userId}`)
+      .then((data) => {
+        setUser(data);
+      })
+      .catch((err) => {
+        console.error(`Error loading /api/user/${userId}`, err);
+      });
+  }, [userId]);
+
+  if (!user) {
+    return <div>Loading user...</div>;
+  }
+
+  return (
+    <div>
+      <Typography variant="h5">
+        {user.first_name} {user.last_name}
+      </Typography>
+      <Typography>Location: {user.location}</Typography>
+      <Typography>Occupation: {user.occupation}</Typography>
+      <Typography>{user.description}</Typography>
+    </div>
+  );
 }
 
 export default UserDetail;
