c4e6a2a8fea16d7dc13e4edc70dbb2d826a7cd18
ManhVD2004 <141122890+ManhVD2004@users.noreply.github.com>
2025-11-22 11:34:50 +0700

    Implement user photo fetching and display
    Fetch and display user photos based on userId from URL.

--- src/components/UserPhotos/index.jsx
+++ src/components/UserPhotos/index.jsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Typography } from "@mui/material";
 
 import "./styles.css";
-import {useParams} from "react-router-dom";
+import fetchModel from "../../lib/fetchModelData";
 
-/**
- * Define UserPhotos, a React component of Project 4.
- */
-function UserPhotos () {
-    const user = useParams();
-    return (
-      <Typography variant="body1">
-        This should be the UserPhotos view of the PhotoShare app. Since it is
-        invoked from React Router the params from the route will be in property
-        match. So this should show details of user:
-        {user.userId}. You can fetch the model for the user
-        from models.photoOfUserModel(userId):
-      </Typography>
-    );
+function UserPhotos() {
+  const { userId } = useParams();
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    fetchModel(`/api/photo/photosOfUser/${userId}`)
+      .then((data) => {
+        setPhotos(data);
+      })
+      .catch((err) => {
+        console.error(`Error loading /api/photo/photosOfUser/${userId}`, err);
+      });
+  }, [userId]);
+
+  if (!photos.length) {
+    return <div>No photos (or loading...)</div>;
+  }
+
+  return (
+    <div>
+      {photos.map((photo) => (
+        <div key={photo._id} style={{ marginBottom: "20px" }}>
+          <Typography variant="subtitle1">
+            {photo.file_name} - {photo.date_time}
+          </Typography>
+          <img
+            src={`/images/${photo.file_name}`}
+            alt={photo.file_name}
+            style={{ maxWidth: "300px", display: "block" }}
+          />
+          <div style={{ marginTop: "8px" }}>
+            {photo.comments &&
+              photo.comments.map((c) => (
+                <div key={c._id}>
+                  <b>
+                    {c.user.first_name} {c.user.last_name}:
+                  </b>{" "}
+                  {c.comment}
+                </div>
+              ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default UserPhotos;
